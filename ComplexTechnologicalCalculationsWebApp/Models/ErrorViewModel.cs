d0c1289c01ac382c8956cbc717a0d52161e12e89
Nameless10000 <116448914+Nameless10000@users.noreply.github.com>
2025-09-28 16:25:13 +0500

    COM-1 : Проект веб приложения

---
+++ ComplexTechnologicalCalculationsWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ComplexTechnologicalCalculationsWebApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
