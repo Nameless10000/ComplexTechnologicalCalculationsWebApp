236a13521f334dc1570c78e2664cd3b5380a22a4
Quazzik <yuakorotkov@gmail.com>
2025-10-02 18:54:36 +0500

    DB-1, бозовые модели сущностей, контекст SlagMode

---
+++ Core/Models/Entity.cs
@@ -0,0 +1,20 @@
+﻿using Core.Models.Auth;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Models
+{
+    public class Entity
+    {
+        [Key]
+        public int Id { get; set; }
+        public DateTime CreationDateTime { get; set; }
+        public int CreatorID { get; set; }
+        public DateTime? LastEditTime { get; set; }
+        public int? LastEditorID { get; set; }
+    }
+}
