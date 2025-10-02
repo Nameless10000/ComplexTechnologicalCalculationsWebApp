e7b85917085e22b9575667852637e9441680fa18
Quazzik <yuakorotkov@gmail.com>
2025-10-02 19:16:38 +0500

    DB-1, Контексты для всех баз

---
+++ Core/Contexts/MatBalDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Contexts
+{
+    public class MatBalDBContext (DbContextOptions<MatBalDBContext> opts) : DbContext(opts)
+    {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Core/Contexts/TModeDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Contexts
+{
+    public class TModeDBContext (DbContextOptions<TModeDBContext> opts) : DbContext(opts)
+    {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
